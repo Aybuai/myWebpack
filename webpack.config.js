30bf1f4f15939d6815587ae0fe8f58eb23903d3a
Aybuai <13624448268@163.com>
2023-03-19 21:26:44 +0800

    feat: babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,23 @@
-'use strict'
+"use strict";
 
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    mode: 'production'
-}
+  entry: {
+    index: "./src/index.js",
+    search: "./src/search.js"
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: "babel-loader",
+      },
+    ],
+  },
+};
