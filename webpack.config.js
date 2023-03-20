a3feb75eb79d65bb1489f42fe935ffef67ef6d20
Aybuai <13624448268@163.com>
2023-03-20 22:44:52 +0800

    feat: style-loader css-loader less-loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,11 @@ const path = require("path");
 module.exports = {
   entry: {
     index: "./src/index.js",
-    search: "./src/search.js"
+    search: "./src/search.js",
   },
   output: {
     path: path.join(__dirname, "dist"),
+    // 多入口文件的话，把固定导出文件名改成 [name].js 用[]占位符去分别生成输出文件
     filename: "[name].js",
   },
   mode: "production",
@@ -17,6 +18,15 @@ module.exports = {
       {
         test: /.js$/,
         use: "babel-loader",
+      },
+      {
+        test: /.css$/,
+        // loader是链式调用，执行顺序是从右到左
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /.less$/,
+        use: ["style-loader", "css-loader", "less-loader"],
       },
     ],
   },
