d48400626555cb6de2c1397a6478a1d0a5489f42
Aybuai <13624448268@163.com>
2023-03-21 20:41:19 +0800

    feat: 压缩文件

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@
 
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
   entry: {
